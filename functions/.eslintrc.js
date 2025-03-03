c2c72397279837cb5801a19bf586130f9dea67d6
Chris Ndirangu <chrisndirangu54@gmail.com>
2025-03-02 20:39:32 -0800

    zfxxxxc

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     "no-restricted-globals": ["error", "name", "length"],
     "prefer-arrow-callback": "error",
     "quotes": ["error", "double", {"allowTemplateLiterals": true}],
+    "no-unused-vars": "warn", // Change from "error" to "warn"
   },
   overrides: [
     {
